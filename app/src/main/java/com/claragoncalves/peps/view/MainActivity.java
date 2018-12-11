5b6a3dbadf8859650fa5673897ee0164b5c80c96
Clara Goncalves <claragoncalves.dev@gmail.com>
2018-12-11 20:17:55 -0300

    nada

--- app/src/main/java/com/claragoncalves/peps/view/MainActivity.java
+++ app/src/main/java/com/claragoncalves/peps/view/MainActivity.java
@@ -1,12 +1,12 @@
 package com.claragoncalves.peps.view;
 
 import android.support.annotation.NonNull;
+import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.MenuItem;
